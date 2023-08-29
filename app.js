5d05da22aecb46be36a1d38e31f8d377fee7e0b7
klav28 <klav28@gmail.com>
2023-08-29 08:06:34 +0300

    description upd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from 'swagger-ui-express';
+import swaggerUi from "swagger-ui-express";
 
 import usersRouter from "./routes/api/auth-router.js";
 import boardRouters from "./routes/api/board/index.js";
@@ -17,8 +17,7 @@ const swaggerOptions = {
     info: {
       title: "GoIT FS backend",
       version: "0.1.0",
-      description:
-        "",
+      description: "",
       license: {
         name: "MIT",
         url: "https://spdx.org/licenses/MIT.html",
@@ -33,7 +32,12 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: ["./routes/api/board/*.js", "./routes/api/*.js", "./models/*.js", "./helpers/HttpError.js"],
+  apis: [
+    "./routes/api/board/*.js",
+    "./routes/api/*.js",
+    "./models/*.js",
+    "./helpers/HttpError.js",
+  ],
 };
 
 const specs = swaggerJSDoc(swaggerOptions);
@@ -41,9 +45,13 @@ const specs = swaggerJSDoc(swaggerOptions);
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, { explorer: true }));
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 app.use("/api/users", usersRouter);
 app.use("/api/board", boardRouters);
 
